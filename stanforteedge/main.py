3341f9f53915533c68771f7423893b01a11fb63d
kokoserver <owonikokoolaoluwa@gmail.com>
2022-06-14 05:34:23 +0100

    update navbar on mobile

--- stanforteedge/main.py
+++ stanforteedge/main.py
@@ -1,7 +1,7 @@
 from starlette.applications import Starlette
 from stanforteedge.core import settings
 from stanforteedge.src.app.base import base_router
-from stanforteedge.db.config import app_startup
+# from stanforteedge.db.config import app_startup
 from stanforteedge.core import middleware
 
 app = Starlette(
@@ -9,7 +9,7 @@ app = Starlette(
 )
 
 
-@app.on_event("startup")
-def start_up():
-    app_startup(app)
-    print("app is starting up")
+# @app.on_event("startup")
+# def start_up():
+#     app_startup(app)
+#     print("app is starting up")
